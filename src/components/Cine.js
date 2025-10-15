990474c39a4f041e296b1f8f2b5f80f0718c9da8
Jorgerdzz <jorgerodriguezalonso20@gmail.com>
2025-10-15 13:54:59 +0200

    practica router realizada

---
+++ src/components/Cine.js
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class Cine extends Component {
+    render () {
+        return(
+            <div>
+                <h1 style={{color: "red"}}>Estoy en cine</h1>
+            </div>
+        )
+    }
+}
+
+export default Cine;
---
+++ src/components/Home.js
@@ -0,0 +1,15 @@
+const { Component } = require("react");
+
+class Home extends Component {
+    
+    render () {
+        return (
+            <div>
+                <h1 style={{color: "blue"}}>Estoy en home</h1>
+            </div>
+        )
+    }
+
+}
+
+export default Home;
